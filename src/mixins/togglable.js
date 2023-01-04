2ccc398efa8f1179e04a6c5cd7aa7ea64a99ebec
Maxime Moreillon <m.moreillon@gmail.com>
2023-01-04 09:26:49 +0900

    toggling devices using http

--- src/mixins/togglable.js
+++ src/mixins/togglable.js
@@ -1,21 +1,20 @@
-import DeviceState from '@/mixins/DeviceState.js'
+import DeviceState from "@/mixins/DeviceState.js";
 
 export const togglable = {
   mixins: [DeviceState],
   methods: {
-    toggle () {
-      var message = {}
-      message.command_topic = this.device.command_topic
+    async toggle() {
+      this.$set(this.device, "loading", true);
 
-      // Just send the opposite state
-      if (this.device_is_on) message.payload = this.device.payload_off
-      else message.payload = this.device.payload_on
+      const { command_topic: topic, payload_off, payload_on } = this.device;
+      const payload = this.device_is_on ? payload_off : payload_on;
 
-      this.$socket.client.emit('front_to_mqtt', message)
-
-      // Mark device as loading
-      this.$set(this.device, 'loading', true)
-    }
-  }
-
-}
+      try {
+        await this.axios.post(`/mqtt`, { topic, payload });
+      } catch (error) {
+        console.error(error);
+        alert("Failed to toggle device");
+      }
+    },
+  },
+};
